025a983e87c6e7fa643605c1b2d59f354949d59d
telkomdev-egi <egiwibowo13@gmail.com>
2023-12-01 16:05:23 +0700

    change url socket

--- chatapprn/socket.tsx
+++ chatapprn/socket.tsx
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 // Replace 'http://localhost:3000' with your server address
-const URL = 'http://192.168.1.8:3000'; // 192.168.1.8 // 103.82.92.193
+const URL = 'http://103.82.92.193:3000'; // 192.168.1.8 -> local // 103.82.92.193 -> vm
 export const socket = io(URL, {autoConnect: false});
 
 socket.onAny((event, ...args) => {
